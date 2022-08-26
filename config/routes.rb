10ff5b6dc421f62fc12125554fe33dcfa7f606bb
Sergey <ser_8904@mail.ru>
2022-08-26 18:42:09 +0300

    add namespace charts

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,15 @@ Rails.application.routes.draw do
   resources :users, only: %i[index edit show update]
   get 'activity', to: 'home#activity'
   get 'analytics', to: 'home#analytics'
-  get 'charts/users_per_day', to: 'charts#users_per_day'
-  get 'charts/enrollments_per_day', to: 'charts#enrollments_per_day'
-  get 'charts/course_popularity', to: 'charts#course_popularity'
+
+  namespace :charts do
+    get 'users_per_day'
+    get 'enrollments_per_day'
+    get 'course_popularity'
+    get 'money_makers'
+  end
+  # get 'charts/users_per_day', to: 'charts#users_per_day'
+  # get 'charts/enrollments_per_day', to: 'charts#enrollments_per_day'
+  # get 'charts/course_popularity', to: 'charts#course_popularity'
+  # get 'charts/money_makers', to: 'charts#money_makers'
 end
