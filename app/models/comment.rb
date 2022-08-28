5c3d569c2b2ddf14126ad47a531afdc736935b55
Sergey <ser_8904@mail.ru>
2022-08-28 15:10:01 +0300

    public activity for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,17 @@
 # frozen_string_literal: true
 
 class Comment < ApplicationRecord
+  include PublicActivity::Model
+  tracked owner: proc { |controller, _model| controller.current_user }
+
   belongs_to :user, counter_cache: true
   belongs_to :lesson, counter_cache: true
   # User.find_each { |user| User.reset_counters(user.id, :comments) }
   # Lesson.find_each { |lesson| Lesson.reset_counters(lesson.id, :comments) }
   validates :content, presence: true
+
+  def to_s
+    content
+  end
+
 end
