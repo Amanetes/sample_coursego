3eb0298c4c055397ff72ee32ef81faa183fdd9a7
Sergey <ser_8904@mail.ru>
2022-08-28 15:34:03 +0300

    add counter_cache for user_lessons to lessons and users

--- app/models/user_lesson.rb
+++ app/models/user_lesson.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class UserLesson < ApplicationRecord
-  belongs_to :user
-  belongs_to :lesson
+  belongs_to :user, counter_cache: true
+  belongs_to :lesson, counter_cache: true
 
   validates :user, presence: true
   validates :lesson, presence: true
