ec268cf1053dd6ba65bcf0b6a32b5a934f37ff70
Sergey <ser_8904@mail.ru>
2022-08-31 14:12:49 +0300

    refactor omniauth login

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,37 +16,25 @@ class User < ApplicationRecord
     email
   end
 
-  # Google Omniauth doesn't support raw addresses so use localhost aka 127.0.0.1 to log in
   def self.from_omniauth(access_token)
     data = access_token.info
     user = User.where(email: data['email']).first
 
-    # Uncomment the section below if you want users to be created if they don't exist
-    if user # if user account exists - add additional data
-      user.name = access_token.info.name
-      user.image = access_token.info.image
-      user.provider = access_token.provider
-      user.uid = access_token.uid
-      user.token = access_token.credentials.token
-      user.expires_at = access_token.credentials.expires_at
-      user.expires = access_token.credentials.expires
-      user.refresh_token = access_token.credentials.refresh_token
-      user.save!
-    else
-      user = User.create(
-        email: data['email'],
-        name: access_token.info.name,
-        image: access_token.info.image,
-        provider: access_token.provider,
-        uid: access_token.uid,
-        token: access_token.credentials.token,
-        expires_at: access_token.credentials.expires_at,
-        expires: access_token.credentials.expires,
-        refresh_token: access_token.credentials.refresh_token,
-        password: Devise.friendly_token[0,20],
-        confirmed_at: Time.zone.now # autoconfirm user from omniauth
-      )
-    end
+    user ||= User.create(
+      email: data['email'],
+      password: Devise.friendly_token[0, 20]
+    )
+
+    user.name = access_token.info.name
+    user.image = access_token.info.image
+    user.provider = access_token.provider
+    user.uid = access_token.uid
+    user.token = access_token.credentials.token
+    user.expires_at = access_token.credentials.expires_at
+    user.expires = access_token.credentials.expires
+    user.refresh_token = access_token.credentials.refresh_token
+    user.confirmed_at = Time.zone.now # autoconfirm user from omniauth
+
     user
   end
 
